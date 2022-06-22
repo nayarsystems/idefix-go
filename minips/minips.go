14e70766c98700aeba5f64108a3ee31df290a8af
Jaime <jbreva@nayarsystems.com>
2022-06-22 11:19:19 +0200

    minips: added subscriber element

--- minips/minips.go
+++ minips/minips.go
@@ -1,25 +1,97 @@
 package minips
 
 import (
+	"context"
 	"fmt"
 	"regexp"
 	"sync"
+	"time"
 )
 
 type Minips[T any] struct {
+	ctx    context.Context
+	cancel context.CancelFunc
 	m      sync.RWMutex
 	topics map[string][]chan T
 }
 
-func NewMinips[T any]() *Minips[T] {
+type Subscriber[T any] struct {
+	ch     chan T
+	mp     *Minips[T]
+	closed bool
+}
+
+func (mp *Minips[T]) NewSubscriber(capacity uint, topics ...string) *Subscriber[T] {
+	s := &Subscriber[T]{}
+	s.mp = mp
+	s.ch = make(chan T, capacity)
+
+	for _, topic := range topics {
+		s.Subscribe(topic)
+	}
+	return s
+}
+
+func (s *Subscriber[T]) Channel() <-chan T {
+	return s.ch
+}
+
+func (s *Subscriber[T]) WaitOne(timeout time.Duration) (T, error) {
+	ctx, cancel := context.WithDeadline(s.mp.ctx, time.Now().Add(timeout))
+	defer cancel()
+
+	var zeroT T
+	for {
+		select {
+		case <-ctx.Done():
+			return zeroT, fmt.Errorf("timeout")
+
+		case m, ok := <-s.ch:
+			if !ok {
+				return zeroT, fmt.Errorf("closed")
+			}
+			return m, nil
+		}
+	}
+}
+
+func (s *Subscriber[T]) Subscribe(topics ...string) error {
+	if s.closed {
+		return fmt.Errorf("closed")
+	}
+
+	for _, topic := range topics {
+		s.mp.registerChannel(topic, s.ch)
+	}
+	return nil
+}
+
+func (s *Subscriber[T]) Unsubscribe(topics ...string) {
+	for _, topic := range topics {
+		s.mp.unregisterChannel(topic, s.ch)
+	}
+}
+
+func (s *Subscriber[T]) UnsubscribeAll() {
+	s.mp.unregisterChannelFromAll(s.ch)
+}
+
+func (s *Subscriber[T]) Close() {
+	s.closed = true
+	s.mp.unregisterChannelFromAll(s.ch)
+	close(s.ch)
+}
+
+func NewMinips[T any](pctx context.Context) *Minips[T] {
 	mp := &Minips[T]{
 		topics: make(map[string][]chan T),
 	}
+	mp.ctx, mp.cancel = context.WithCancel(pctx)
 
 	return mp
 }
 
-func (mp *Minips[T]) RegisterChannel(topic string, ch chan T) {
+func (mp *Minips[T]) registerChannel(topic string, ch chan T) {
 	mp.m.Lock()
 	defer mp.m.Unlock()
 
@@ -31,7 +103,7 @@ func (mp *Minips[T]) RegisterChannel(topic string, ch chan T) {
 	mp.topics[topic] = append(list, ch)
 }
 
-func (mp *Minips[T]) UnregisterChannel(topic string, ch chan T) {
+func (mp *Minips[T]) unregisterChannel(topic string, ch chan T) {
 	mp.m.Lock()
 	defer mp.m.Unlock()
 
@@ -55,7 +127,7 @@ func (mp *Minips[T]) unregisterChannelSafe(topic string, ch chan T) {
 	}
 }
 
-func (mp *Minips[T]) UnregisterChannelFromAll(ch chan T) {
+func (mp *Minips[T]) unregisterChannelFromAll(ch chan T) {
 	mp.m.RLock()
 	defer mp.m.RUnlock()
 
@@ -106,4 +178,8 @@ func (mp *Minips[T]) publishTopic(topic string, elem T) {
 		default:
 		}
 	}
+}
+
+func (mp *Minips[T]) Close() {
+	mp.cancel()
 }
--- minips/minips_test.go
+++ minips/minips_test.go
@@ -1,21 +1,23 @@
 package minips
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestMinips(t *testing.T) {
-	mp := NewMinips[string]()
+	mp := NewMinips[string](context.Background())
 
 	ch1 := make(chan string, 100)
 	ch11 := make(chan string, 100)
 	ch2 := make(chan string, 100)
 
-	mp.RegisterChannel("test1", ch1)
-	mp.RegisterChannel("test2", ch2)
-	mp.RegisterChannel("test1", ch11)
+	mp.registerChannel("test1", ch1)
+	mp.registerChannel("test2", ch2)
+	mp.registerChannel("test1", ch11)
 
 	mp.Publish("test1.asdf.qwe", "hola")
 	mp.Publish("test1.asdf", "hola")
@@ -26,42 +28,82 @@ func TestMinips(t *testing.T) {
 	require.Equal(t, 3, len(ch11))
 	require.Equal(t, 1, len(ch2))
 
-	mp.UnregisterChannel("test2", ch2)
+	mp.unregisterChannel("test2", ch2)
 	mp.Publish("test2", "hola")
 	require.Equal(t, 1, len(ch2))
 
 	close(ch2)
-	mp.RegisterChannel("test2", ch2)
+	mp.registerChannel("test2", ch2)
 	mp.Publish("test2", "hola") // This will panic->recover
 
-	mp.UnregisterChannel("test2", ch2)
-	mp.UnregisterChannel("test1", ch1)
-	mp.UnregisterChannel("test1", ch11)
+	mp.unregisterChannel("test2", ch2)
+	mp.unregisterChannel("test1", ch1)
+	mp.unregisterChannel("test1", ch11)
 	require.Equal(t, 0, len(mp.topics))
 }
 
 func TestMinipsInt(t *testing.T) {
-	mp := NewMinips[int]()
+	mp := NewMinips[int](context.Background())
 
 	ch1 := make(chan int, 100)
 
-	mp.RegisterChannel("test1", ch1)
+	mp.registerChannel("test1", ch1)
 
 	mp.Publish("test1.asdf.qwe", 123)
 	require.Equal(t, 1, len(ch1))
 }
 
 func TestMinipsUnregAll(t *testing.T) {
-	mp := NewMinips[int]()
+	mp := NewMinips[int](context.Background())
 
 	ch1 := make(chan int, 100)
 
-	mp.RegisterChannel("test1", ch1)
-	mp.RegisterChannel("test2", ch1)
-	mp.RegisterChannel("test3", ch1)
-	mp.RegisterChannel("test4", ch1)
+	mp.registerChannel("test1", ch1)
+	mp.registerChannel("test2", ch1)
+	mp.registerChannel("test3", ch1)
+	mp.registerChannel("test4", ch1)
 
-	mp.UnregisterChannelFromAll(ch1)
+	mp.unregisterChannelFromAll(ch1)
 
 	require.Equal(t, 0, len(mp.topics))
+}
+
+func TestSubscriber(t *testing.T) {
+	mp := NewMinips[int](context.Background())
+
+	s := mp.NewSubscriber(10, "test1", "test2", "test3")
+	mp.Publish("test1", 123)
+	mp.Publish("test2", 456)
+	mp.Publish("test4", 0)
+
+	require.Equal(t, 2, len(s.Channel()))
+	require.Equal(t, 123, <-s.Channel())
+
+	n, err := s.WaitOne(time.Duration(time.Second))
+	require.NoError(t, err)
+	require.Equal(t, 456, n)
+
+	err = s.Subscribe("test4")
+	require.NoError(t, err)
+	_, err = s.WaitOne(time.Duration(time.Millisecond * 100))
+	require.Error(t, err)
+
+	s.Unsubscribe("test1")
+	mp.Publish("test1", 123)
+	_, err = s.WaitOne(time.Duration(time.Millisecond * 100))
+	require.Error(t, err)
+
+	s.UnsubscribeAll()
+	mp.Publish("test2", 123)
+	_, err = s.WaitOne(time.Duration(time.Millisecond * 100))
+	require.Error(t, err)
+
+	s.Close()
+	err = s.Subscribe("test5")
+	require.Error(t, err)
+
+	mp.Close()
+	ns := mp.NewSubscriber(1, "test")
+	_, err = ns.WaitOne(time.Duration(time.Millisecond * 100))
+	require.Error(t, err)
 }
