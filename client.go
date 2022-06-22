179dfe3ed73990b986089a25ce5f201d984c9b6e
Jaime <jbreva@nayarsystems.com>
2022-06-22 11:32:38 +0200

    Rename client variable

--- client.go
+++ client.go
@@ -21,26 +21,26 @@ import (
 )
 
 func NewClient(pctx context.Context, opts *ConnectionOptions) (*Client, error) {
-	im := &Client{
+	c := &Client{
 		opts: opts,
 	}
 
-	if err := im.connect(pctx, opts.BrokerAddress, opts.CACert); err != nil {
+	if err := c.connect(pctx, opts.BrokerAddress, opts.CACert); err != nil {
 		return nil, err
 	}
 
-	if err := im.login(opts.Address, opts.Token, opts.Meta); err != nil {
+	if err := c.login(opts.Address, opts.Token, opts.Meta); err != nil {
 		return nil, err
 	}
 
-	return im, nil
+	return c, nil
 }
 
-func (im *Client) connect(pctx context.Context, brokerAddress string, CACert []byte) (err error) {
-	im.ctx, im.cancelFunc = context.WithCancel(pctx)
-	im.prefix = MqttIdefixPrefix
-	im.ps = minips.NewMinips[*Message](im.ctx)
-	im.compThreshold = 128
+func (c *Client) connect(pctx context.Context, brokerAddress string, CACert []byte) (err error) {
+	c.ctx, c.cancelFunc = context.WithCancel(pctx)
+	c.prefix = MqttIdefixPrefix
+	c.ps = minips.NewMinips[*Message](c.ctx)
+	c.compThreshold = 128
 
 	opts := mqtt.NewClientOptions()
 	opts.AddBroker(brokerAddress)
@@ -57,24 +57,24 @@ func (im *Client) connect(pctx context.Context, brokerAddress string, CACert []b
 		})
 	}
 
-	im.sessionID, err = randSessionID()
+	c.sessionID, err = randSessionID()
 	if err != nil {
 		return err
 	}
-	opts.SetClientID(im.sessionID)
+	opts.SetClientID(c.sessionID)
 
-	opts.SetConnectionLostHandler(im.connectionLostHandler)
-	opts.SetDefaultPublishHandler(im.messageHandler)
+	opts.SetConnectionLostHandler(c.connectionLostHandler)
+	opts.SetDefaultPublishHandler(c.messageHandler)
 
-	im.client = mqtt.NewClient(opts)
+	c.client = mqtt.NewClient(opts)
 
-	token := im.client.Connect()
+	token := c.client.Connect()
 	token.Wait()
 	if token.Error() != nil {
 		return token.Error()
 	}
 
-	token = im.client.Subscribe(fmt.Sprintf("%s/%s/r/+", im.prefix, im.sessionID), 1, nil)
+	token = c.client.Subscribe(fmt.Sprintf("%s/%s/r/+", c.prefix, c.sessionID), 1, nil)
 	token.Wait()
 	if token.Error() != nil {
 		return token.Error()
@@ -83,23 +83,23 @@ func (im *Client) connect(pctx context.Context, brokerAddress string, CACert []b
 	return nil
 }
 
-func (im *Client) responseTopic() string {
-	return fmt.Sprintf("%s/%s/r/", im.prefix, im.sessionID)
+func (c *Client) responseTopic() string {
+	return fmt.Sprintf("%s/%s/r/", c.prefix, c.sessionID)
 }
 
-func (im *Client) publishTopic(flags string) string {
-	return fmt.Sprintf("%s/%s/t/%s", im.prefix, im.sessionID, flags)
+func (c *Client) publishTopic(flags string) string {
+	return fmt.Sprintf("%s/%s/t/%s", c.prefix, c.sessionID, flags)
 }
 
-func (im *Client) login(deviceAddress string, deviceToken string, meta map[string]interface{}) (err error) {
-	im.address = deviceAddress
-	im.token = deviceToken
+func (c *Client) login(deviceAddress string, deviceToken string, meta map[string]interface{}) (err error) {
+	c.address = deviceAddress
+	c.token = deviceToken
 
 	lm := loginMsg{
-		Address:  im.opts.Address,
-		Token:    im.opts.Token,
-		Encoding: im.opts.Encoding,
-		Meta:     im.opts.Meta,
+		Address:  c.opts.Address,
+		Token:    c.opts.Token,
+		Encoding: c.opts.Encoding,
+		Meta:     c.opts.Meta,
 		Time:     time.Now().UnixMilli(),
 	}
 
@@ -108,7 +108,7 @@ func (im *Client) login(deviceAddress string, deviceToken string, meta map[strin
 		Data: lm,
 	}
 
-	_, err = im.Call("idefix", tm, time.Second*3)
+	_, err = c.Call("idefix", tm, time.Second*3)
 	if err != nil {
 		return err
 	}
@@ -125,13 +125,13 @@ func randSessionID() (string, error) {
 	return hex.EncodeToString(b), nil
 }
 
-func (im *Client) sendMessage(tm *Message) (err error) {
+func (c *Client) sendMessage(tm *Message) (err error) {
 	var flags string
 	var marshaled bool
 	var marshalErr error
 	var data []byte
 
-	if strings.Contains(im.opts.Encoding, "j") && !marshaled {
+	if strings.Contains(c.opts.Encoding, "j") && !marshaled {
 		marshaled = true
 		flags += "j"
 		if v, ok := tm.Data.(map[string]interface{}); ok {
@@ -143,7 +143,7 @@ func (im *Client) sendMessage(tm *Message) (err error) {
 		}
 	}
 
-	if strings.Contains(im.opts.Encoding, "m") && !marshaled {
+	if strings.Contains(c.opts.Encoding, "m") && !marshaled {
 		marshaled = true
 		flags += "m"
 		if v, ok := tm.Data.(map[string]interface{}); ok {
@@ -165,7 +165,7 @@ func (im *Client) sendMessage(tm *Message) (err error) {
 
 	var compressed bool
 
-	if strings.Contains(im.opts.Encoding, "g") && !compressed {
+	if strings.Contains(c.opts.Encoding, "g") && !compressed {
 		buf := new(bytes.Buffer)
 		wr := gzip.NewWriter(buf)
 		n, err := wr.Write(data)
@@ -180,13 +180,13 @@ func (im *Client) sendMessage(tm *Message) (err error) {
 		}
 	}
 
-	msg := im.client.Publish(im.publishTopic(flags), 1, false, data)
+	msg := c.client.Publish(c.publishTopic(flags), 1, false, data)
 	msg.Wait()
 	return msg.Error()
 }
 
-func (im *Client) messageHandler(client mqtt.Client, msg mqtt.Message) {
-	if !strings.HasPrefix(msg.Topic(), im.responseTopic()) {
+func (c *Client) messageHandler(client mqtt.Client, msg mqtt.Message) {
+	if !strings.HasPrefix(msg.Topic(), c.responseTopic()) {
 		return
 	}
 
@@ -235,11 +235,11 @@ func (im *Client) messageHandler(client mqtt.Client, msg mqtt.Message) {
 		return
 	}
 
-	if strings.HasPrefix(tm.To, im.address+".") {
+	if strings.HasPrefix(tm.To, c.address+".") {
 		return
 	}
 
-	tm.To = strings.TrimPrefix(tm.To, im.address+".")
+	tm.To = strings.TrimPrefix(tm.To, c.address+".")
 
 	if tm.To == "" {
 		return
@@ -253,9 +253,9 @@ func (im *Client) messageHandler(client mqtt.Client, msg mqtt.Message) {
 		}
 	}
 
-	im.ps.Publish(tm.To, &Message{Response: tm.Res, To: tm.To, Data: tm.Data, Err: tm.Err})
+	c.ps.Publish(tm.To, &Message{Response: tm.Res, To: tm.To, Data: tm.Data, Err: tm.Err})
 }
 
-func (im *Client) connectionLostHandler(client mqtt.Client, err error) {
-	im.cancelFunc()
+func (c *Client) connectionLostHandler(client mqtt.Client, err error) {
+	c.cancelFunc()
 }
--- methods.go
+++ methods.go
@@ -7,36 +7,36 @@ import (
 	"gitlab.com/garagemakers/idefix-go/minips"
 )
 
-func (im *Client) Publish(remoteAddress string, msg *Message) error {
+func (c *Client) Publish(remoteAddress string, msg *Message) error {
 	msg.To = fmt.Sprintf("%s.%s", remoteAddress, msg.To)
-	return im.sendMessage(msg)
+	return c.sendMessage(msg)
 }
 
-func (im *Client) Answer(origmsg *Message, msg *Message) error {
+func (c *Client) Answer(origmsg *Message, msg *Message) error {
 	msg.To = origmsg.Response // TODO: Check this
-	return im.sendMessage(msg)
+	return c.sendMessage(msg)
 }
 
-func (im *Client) Call(remoteAddress string, msg *Message, timeout time.Duration) (interface{}, error) {
+func (c *Client) Call(remoteAddress string, msg *Message, timeout time.Duration) (interface{}, error) {
 	msg.To = fmt.Sprintf("%s.%s", remoteAddress, msg.To)
 	msg.Response, _ = randSessionID()
 
-	sub := im.ps.NewSubscriber(1, msg.Response)
+	sub := c.ps.NewSubscriber(1, msg.Response)
 	defer sub.Close()
 
-	if err := im.sendMessage(msg); err != nil {
+	if err := c.sendMessage(msg); err != nil {
 		return nil, err
 	}
 
 	return sub.WaitOne(timeout)
 }
 
-func (im *Client) NewSubscriber(capacity uint, topic ...string) *minips.Subscriber[*Message] {
-	return im.ps.NewSubscriber(capacity, topic...)
+func (c *Client) NewSubscriber(capacity uint, topic ...string) *minips.Subscriber[*Message] {
+	return c.ps.NewSubscriber(capacity, topic...)
 }
 
-func (im *Client) WaitOne(topic string, timeout time.Duration) (*Message, error) {
-	sub := im.ps.NewSubscriber(1, topic)
+func (c *Client) WaitOne(topic string, timeout time.Duration) (*Message, error) {
+	sub := c.ps.NewSubscriber(1, topic)
 	defer sub.Close()
 	return sub.WaitOne(timeout)
 }
