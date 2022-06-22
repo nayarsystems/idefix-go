d66532d1f2ad4f8ca7bd7b505faf368874975a67
Jaime <jbreva@nayarsystems.com>
2022-06-22 13:17:17 +0200

    client: added streams

--- client_test.go
+++ client_test.go
@@ -45,3 +45,35 @@ func TestUnauthorized(t *testing.T) {
 
 	require.Error(t, err)
 }
+
+func TestStream(t *testing.T) {
+	c, err := NewClient(context.Background(), &ConnectionOptions{
+		BrokerAddress: "tcp://localhost:1883",
+		Encoding:      "mg",
+		CACert:        cert.CaCert,
+		Address:       "test",
+		Token:         "token",
+	})
+	require.NoError(t, err)
+
+	c2, err := NewClient(context.Background(), &ConnectionOptions{
+		BrokerAddress: "tcp://localhost:1883",
+		Encoding:      "mg",
+		CACert:        cert.CaCert,
+		Address:       "test2",
+		Token:         "token",
+	})
+
+	require.NoError(t, err)
+
+	s, err := c.NewStream("5c9719505534d914", "asdf", 100, time.Minute)
+	require.NoError(t, err)
+	defer s.Close()
+
+	err = c2.Publish("5c9719505534d914", &Message{To: "asdf", Data: "test"})
+	require.NoError(t, err)
+
+	e := <-s.Channel()
+	require.Equal(t, "test", e.Data)
+
+}
---
+++ stream.go
@@ -0,0 +1,86 @@
+package idefixgo
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"gitlab.com/garagemakers/idefix-go/minips"
+)
+
+type Stream struct {
+	ctx     context.Context
+	cancel  context.CancelFunc
+	timeout time.Duration
+	c       *Client
+	sub     *minips.Subscriber[*Message]
+	topic   string
+	address string
+}
+
+func (c *Client) NewStream(address string, topic string, capacity uint, timeout time.Duration) (*Stream, error) {
+	s := &Stream{
+		address: address,
+		timeout: timeout,
+		topic:   topic,
+		c:       c,
+		sub:     c.ps.NewSubscriber(capacity),
+	}
+
+	if err := s.sub.Subscribe(s.subTopic()); err != nil {
+		return nil, err
+	}
+
+	s.ctx, s.cancel = context.WithCancel(c.ctx)
+
+	_, err := s.c.Call(address, &Message{To: s.openTopic()}, time.Second*5)
+	if err != nil {
+		return nil, err
+	}
+
+	go s.keepalive()
+
+	return s, nil
+}
+
+func (s *Stream) openTopic() string {
+	return "$ss." + s.topic
+}
+
+func (s *Stream) subTopic() string {
+	return fmt.Sprintf("$sm.%s.%s", s.address, s.topic)
+}
+
+func (s *Stream) closeTopic() string {
+	return "$su." + s.topic
+}
+
+func (s *Stream) keepalive() {
+	t := time.NewTicker(s.timeout / 2)
+	defer s.Close()
+
+	for {
+		select {
+		case <-s.ctx.Done():
+			return
+		case <-t.C:
+			_, err := s.c.Call(s.address, &Message{To: s.openTopic()}, s.timeout)
+			if err != nil {
+				return
+			}
+		}
+	}
+}
+
+func (s *Stream) Channel() <-chan *Message {
+	return s.sub.Channel()
+}
+
+func (s *Stream) Close() error {
+	defer s.cancel()
+	_, err := s.c.Call(s.address, &Message{To: s.closeTopic()}, s.timeout)
+	if err != nil {
+		return err
+	}
+	return nil
+}
