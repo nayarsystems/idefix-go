0cebcfb4ca2de4a4a14a0d3dd6c2c44ea2dbc1fc
Jaime <jbreva@nayarsystems.com>
2022-09-22 18:42:11 +0200

    tools/idefix: Added stream command

--- tools/idefix/cmd_stream.go
+++ tools/idefix/cmd_stream.go
@@ -14,14 +14,22 @@ func init() {
 	cmdLog.MarkFlagRequired("device")
 	cmdLog.Flags().BoolP("wait", "w", false, "Wait for the device if not connected")
 	cmdLog.Flags().IntP("loglevel", "l", 2, "Filter lower log levels")
-
 	rootCmd.AddCommand(cmdLog)
+
+	cmdStream.Flags().StringP("device", "d", "", "Device ID")
+	rootCmd.AddCommand(cmdStream)
 }
 
 var cmdLog = &cobra.Command{
 	Use:   "log",
 	Short: "Stream Device ID sys.evt.log messages",
 	RunE:  cmdLogRunE,
+}
+
+var cmdStream = &cobra.Command{
+	Use:   "stream",
+	Short: "Stream device messages",
+	RunE:  cmdStreamRunE,
 }
 
 func cmdLogRunE(cmd *cobra.Command, args []string) error {
@@ -68,6 +76,41 @@ func cmdLogRunE(cmd *cobra.Command, args []string) error {
 			}
 
 			fmt.Printf("[%d] %s\n", l, m)
+
+		case <-ic.Context().Done():
+			return nil
+		}
+	}
+}
+
+func cmdStreamRunE(cmd *cobra.Command, args []string) error {
+	addr, err := cmd.Flags().GetString("device")
+	if err != nil {
+		return err
+	}
+
+	if len(args) != 1 {
+		return fmt.Errorf("Stream only supports one topic")
+	}
+
+	ic, err := getConnectedClient()
+	if err != nil {
+		return err
+	}
+	defer ic.Disconnect()
+
+	s, err := ic.NewStream(addr, args[0], 100, time.Minute*10)
+	if err != nil {
+		log.Fatalln("Cannot open stream:", err)
+	}
+
+	defer s.Close()
+
+	fmt.Printf("-- Streaming %s %s --\n", addr, args[0])
+	for {
+		select {
+		case k := <-s.Channel():
+			fmt.Printf("%v\n", k.Data)
 
 		case <-ic.Context().Done():
 			return nil
