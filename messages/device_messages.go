fefaf864a7603f066dcd52ce388af186faa6c700
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2023-05-03 18:41:05 +0200

    messages: add messages related to update firmware of physically connected devices

--- messages/device_messages.go
+++ messages/device_messages.go
@@ -14,11 +14,31 @@ type InfoReqMsg struct {
 /*******************/
 
 type DevListReqMsg struct {
-	Expr        string `bson:"expr" json:"expr" msgpack:"expr" mapstructure:"expr,omitempty"`
+	Expr        string `bson:"expr" json:"expr" msgpack:"expr" mapstructure:"expr"`
 	FieldFilter string `bson:"fieldFilter" json:"fieldFilter" msgpack:"fieldFilter" mapstructure:"fieldFilter,omitempty"`
 }
 
 // list of usb devices and their usb attributes/env
 type DevListResponseMsg struct {
 	Devices []map[string]any `bson:"devices" json:"devices" msgpack:"devices" mapstructure:"devices"`
+}
+
+/********************************************************/
+/*   Manage firmware updates over devices physically    */
+/*   connected to a idefix client (e.g: RP2040, STM32)  */
+/********************************************************/
+
+type DevType int
+
+const (
+	DEVTYPE_RPI_PICO DevType = 0
+)
+
+type UpdateDevFirmReqMsg struct {
+	DevType    DevType `bson:"devType" json:"devType" msgpack:"devType" mapstructure:"devType"`
+	FirmUpdate []byte  `bson:"firmUpdate" json:"firmUpdate" msgpack:"firmUpdate" mapstructure:"firmUpdate"`
+}
+
+type UpdateDevFirmResMsg struct {
+	Ok bool `json:"ok" msgpack:"ok" mapstructure:"ok"`
 }
--- messages/device_topics.go
+++ messages/device_topics.go
@@ -7,4 +7,12 @@ const (
 	//
 	// - response: DevListResponseMsg
 	TopicCmdGetDevInfo = "udev.cmd.info"
+
+	// TopicCmdUpdateDevFirm is used to update the firmware
+	// of a device physically connected to a idefix client (e.g: RP2040, STM32)
+	//
+	// - message: UpdateDevFirmReqMsg
+	//
+	// - response: UpdateDevFirmResMsg
+	TopicCmdUpdateDevFirm = "efirm_updater.cmd.update"
 )
