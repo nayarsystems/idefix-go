11b01fd8ea8d5f50bf15299275c9b9a4fb20dd4e
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2024-04-05 17:22:48 +0200

    tools/idefix: allow simple strings and bools in publish cmd

--- tools/idefix/cmd_publish.go
+++ tools/idefix/cmd_publish.go
@@ -12,6 +12,7 @@ import (
 
 func init() {
 	cmdPublish.Flags().StringP("address", "a", "", "Device address")
+	cmdPublish.Flags().BoolP("bool", "b", false, "Is boolean")
 	cmdPublish.MarkFlagRequired("address")
 
 	rootCmd.AddCommand(cmdPublish)
@@ -19,13 +20,17 @@ func init() {
 
 var cmdPublish = &cobra.Command{
 	Use:   "publish",
-	Short: "Publish a map to the device",
+	Short: "Publish a value to the device",
 	Args:  cobra.MinimumNArgs(1),
 	RunE:  cmdPublishRunE,
 }
 
 func cmdPublishRunE(cmd *cobra.Command, args []string) error {
 	addr, err := cmd.Flags().GetString("address")
+	if err != nil {
+		return err
+	}
+	isBool, err := cmd.Flags().GetBool("bool")
 	if err != nil {
 		return err
 	}
@@ -36,15 +41,32 @@ func cmdPublishRunE(cmd *cobra.Command, args []string) error {
 	}
 	defer ic.Disconnect()
 
-	amap := make(map[string]interface{})
+	var value any
 	if len(args) > 1 {
-		if err := json.Unmarshal([]byte(strings.Join(args[1:], " ")), &amap); err != nil {
-			return err
+		if isBool {
+			if args[1] == "true" || args[1] == "True" || args[1] == "TRUE" || args[1] == "1" {
+				value = true
+			} else if args[1] == "false" || args[1] == "False" || args[1] == "FALSE" || args[1] == "0" {
+				value = false
+			} else {
+				return fmt.Errorf("invalid boolean value")
+			}
+		} else {
+			amap := make(map[string]interface{})
+			if err := json.Unmarshal([]byte(strings.Join(args[1:], " ")), &amap); err != nil {
+				if len(args) > 2 {
+					return fmt.Errorf("invalid arguments")
+				}
+				value = args[1]
+			} else {
+				// Consider a string value
+				value = amap
+			}
 		}
 	}
 
 	spinner, _ := pterm.DefaultSpinner.WithShowTimer(true).Start("Publishing on ", args[0])
-	if err := ic.Publish(addr, &m.Message{To: args[0], Data: amap}); err != nil {
+	if err := ic.Publish(addr, &m.Message{To: args[0], Data: value}); err != nil {
 		spinner.Fail()
 		return fmt.Errorf("cannot publish the message to the device: %w", err)
 	}
