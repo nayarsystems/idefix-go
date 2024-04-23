504b1d1584c7f08d97ae61f0a40b9d52c94d861d
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2024-04-23 16:13:09 +0200

    tools/idefix: add revision

--- tools/idefix/main.go
+++ tools/idefix/main.go
@@ -2,20 +2,46 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
 	"time"
 
 	idf "github.com/nayarsystems/idefix-go"
 	"github.com/spf13/cobra"
+	"runtime/debug"
 )
 
 var rootctx context.Context
 var cancel context.CancelFunc
+var version string = getVersion()
+
+// https://tip.golang.org/doc/go1.18#debug_buildinfo
+// https://blog.carlana.net/post/2023/golang-git-hash-how-to/
+func getVersion() (v string) {
+	v = "unknown"
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		return
+	}
+	dirty := false
+	for _, kv := range info.Settings {
+		switch kv.Key {
+		case "vcs.revision":
+			v = kv.Value[:8]
+		case "vcs.modified":
+			dirty = kv.Value == "true"
+		}
+	}
+	if dirty {
+		v += "-dirty"
+	}
+	return
+}
 
 var rootCmd = &cobra.Command{
 	Use:          "idefix",
-	Short:        "idefix multi-tool",
+	Short:        fmt.Sprintf("idefix multi-tool (version: %s)", version),
 	SilenceUsage: true,
 }
 
