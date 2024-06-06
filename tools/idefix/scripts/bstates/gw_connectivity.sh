21e17687f0bac969536acc628281c15a12aba746
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2024-06-06 11:23:54 +0200

    scripts: improve connectivity

--- tools/idefix/scripts/bstates/gw_connectivity.sh
+++ tools/idefix/scripts/bstates/gw_connectivity.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
-# Parameters: <address> <from-date>
-if [ "$#" -ne 2 ]; then
+# Parameters: <address> <from-date> [<extrargs>, ...]
+if [ "$#" -lt 2 ]; then
     echo "This script is used to show the connectivity status of a gateway from a specific date."
     echo "This perform calls to get bstates events pushed by the specified address." 
-    echo "Usage: $0 <address> <from-date>"
+    echo "Usage: $0 <address> <from-date> [<extrargs>, ...]"
     echo "Example: $0 61abdc949d199f2f \"2024-05-30 10:07:00 +0200 CEST\" "
     exit 1
+fi
+
+# Optional parameters
+extrargs=""
+if [ "$#" -gt 2 ]; then
+    extrargs="${@:3}"
 fi
 
 address=$1
 date=$2
 
-idefix event get bstates -a ${address} --since="${date}" --timeout 60s --meta-filter {\"source_type\":\"gateway\"} --field-align --continue \
+idefix event get bstates -a ${address} --since="${date}" --timeout 60s --meta-filter {\"source_type\":\"gateway\"} ${extrargs} \
     --field-alias BOOT_COUNTER=00_BC \
     --field-alias EVENT_COUNTER=01_EC \
     --field-alias CSQ=02_CSQ \
@@ -22,7 +28,7 @@ idefix event get bstates -a ${address} --since="${date}" --timeout 60s --meta-fi
     --field-alias OPERATOR=06_OPERATOR \
     --field-alias CREG=07_CREG \
     --field-alias CPSI_OPMODE=08_OperationMode \
-    --field-alias CPSI_MCC=09_MMC \
+    --field-alias CPSI_MCC=09_MCC \
     --field-alias CPSI_MNC=10_MNC \
     --field-alias CPSI_CELLID=11_CELLID \
     --field-alias CPSI_PCELLID=12_PCELLID \
