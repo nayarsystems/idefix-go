680e529edff7dd4089089e2a679b9c63ca758767
Diego Centelles Beltran <dcentelles@nayarsystems.com>
2024-06-24 09:18:56 +0200

    idefix: add sync-now param to config cmd

--- tools/idefix/cmd_config_update.go
+++ tools/idefix/cmd_config_update.go
@@ -5,12 +5,13 @@ import (
 	"os"
 
 	m "github.com/nayarsystems/idefix-go/messages"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
 func init() {
 	cmdConfigUpdate.Flags().StringP("file", "f", "", "New configuration file")
-	cmdConfigUpdate.MarkFlagRequired("file")
+	cmdConfigUpdate.Flags().Bool("sync-now", false, "force the client to sync the configuration immediately")
 
 	cmdConfig.AddCommand(cmdConfigUpdate)
 }
@@ -29,26 +30,55 @@ func cmdConfigUpdateRunE(cmd *cobra.Command, args []string) (err error) {
 	if err != nil {
 		return err
 	}
-	newConfig, err := os.ReadFile(file)
+	forceSync, err := cmd.Flags().GetBool("sync-now")
 	if err != nil {
-		return fmt.Errorf("cannot read file %s: %w", file, err)
+		return err
 	}
+	if file == "" && !forceSync {
+		return fmt.Errorf("at least either a new configuration file or the --sync-now flag must be provided")
+	}
+
+	spinner, err := pterm.DefaultSpinner.Start("connecting...")
+	if err != nil {
+		return err
+	}
+	defer spinner.Stop()
 
 	ic, err := getConnectedClient()
 	if err != nil {
 		return err
 	}
 	defer ic.Disconnect()
+	spinner.Success("connected")
 
-	msg := &m.AddressConfigUpdateMsg{
-		Address: conf.address,
-		Config:  newConfig,
+	if file != "" {
+		newConfig, err := os.ReadFile(file)
+		if err != nil {
+			return fmt.Errorf("cannot read file %s: %w", file, err)
+		}
+
+		spinner.UpdateText("uploading configuration file...")
+		msg := &m.AddressConfigUpdateMsg{
+			Address: conf.address,
+			Config:  newConfig,
+		}
+		var res m.AddressConfigUpdateResponseMsg
+		err = ic.Call2(m.IdefixCmdPrefix, &m.Message{To: m.CmdAddressConfigUpdate, Data: msg}, &res, getTimeout(cmd))
+		if err != nil {
+			return fmt.Errorf("cannot upload client configuration: %w", err)
+		}
+		spinner.Success("configuration file uploaded successfully")
 	}
-	var res m.AddressConfigUpdateResponseMsg
-	err = ic.Call2(m.IdefixCmdPrefix, &m.Message{To: m.CmdAddressConfigUpdate, Data: msg}, &res, getTimeout(cmd))
-	if err != nil {
-		return fmt.Errorf("cannot update client configuration: %w", err)
+
+	if forceSync {
+		spinner.UpdateText("force client to sync its configuration...")
+		var syncRes m.SyncConfigResponseMsg
+		err = ic.Call2(conf.address, &m.Message{To: m.TopicCmdSyncConfig, Data: m.SyncConfigReqMsg{}}, &syncRes, getTimeout(cmd))
+		if err != nil {
+			return fmt.Errorf("cannot force client to sync its configuration: %w", err)
+		}
+		spinner.Success(syncRes.Result)
 	}
-	fmt.Println("configuration file updated successfully")
+	fmt.Println()
 	return nil
 }
