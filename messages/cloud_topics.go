cb9b19cbf6fe2b9fe4478fea6cb47a393bba74bc
Carlos del Olmo <c.delolmo@nayarsystems.com>
2024-10-01 15:59:07 +0200

    cloud_topics: improved topic descriptions

--- messages/cloud_topics.go
+++ messages/cloud_topics.go
@@ -97,110 +97,111 @@ const (
 	// TopicTransportAddressAliasRemove is used to remove an alias from an address
 	TopicTransportAddressAliasRemove = TopicTransportIdefixPrefix + CmdAddressAliasRemove
 
-	// Mqtt prefix for idefix project
+	// MqttIdefixPrefix represents the MQTT prefix for the Idefix project
 	MqttIdefixPrefix = "ifx"
 
-	// Mqtt prefix for public messages
+	// MqttPublicPrefix represents the MQTT prefix for public messages
 	MqttPublicPrefix = "p"
 
-	// IdefixPrefix is the prefix for all cloud system commands like login
+	// IdefixCmdPrefix is the base prefix for all cloud system commands related to Idefix, such as login
 	IdefixCmdPrefix = "idefix"
 
-	// TopicTransportIdefixPrefix is the prefix for all cloud system commands like login
+	// TopicTransportIdefixPrefix is the MQTT topic prefix for all cloud system commands under the Idefix namespace
 	TopicTransportIdefixPrefix = IdefixCmdPrefix + "."
 
-	// CmdLogin is the login function
+	// CmdLogin is the command for user login
 	CmdLogin = "login"
 
-	// CmdDomainCreate is the cereate domain function
+	// CmdDomainCreate is the command to create a new domain
 	CmdDomainCreate = "domain.create"
 
-	// CmdDomainUpdate is the update domain function
+	// CmdDomainUpdate is the command to update an existing domain
 	CmdDomainUpdate = "domain.update"
 
-	// CmdDomainUpdateAccessRules is the update domain's access rules function
+	// CmdDomainUpdateAccessRules is the command to update the access rules of a domain
 	CmdDomainUpdateAccessRules = "domain.rules.update"
 
-	// CmdDomainDelete is the delete domain function
+	// CmdDomainDelete is the command to delete a domain
 	CmdDomainDelete = "domain.delete"
 
-	// CmdDomainGet is the get domain function
+	// CmdDomainGet is the command to retrieve details of a specific domain
 	CmdDomainGet = "domain.get"
 
-	// CmdDomainAssign is the assign domain function
+	// CmdDomainAssign is the command to assign an address to a domain
 	CmdDomainAssign = "domain.assign"
 
-	// CmdDomainListAddresses list every address assigned to domains under the specified domain
+	// CmdDomainListAddresses is the command to list all addresses associated with a specific domain
 	CmdDomainListAddresses = "domain.list.addresses"
 
-	// CmdDomainListGroups list every group under the specified domain
+	// CmdDomainListGroups is the command to list all groups within a specific domain
 	CmdDomainListGroups = "domain.list.groups"
 
-	// CmdDomainCountAddresses count every address assigned to domains under the specified domain
+	// CmdDomainCountAddresses is the command to count the addresses associated with a specific domain
 	CmdDomainCountAddresses = "domain.count.addresses"
 
-	// CmdDomainTree list every nested domain under the specified domain
+	// CmdDomainTree is the command to list all nested domains under a specific domain
 	CmdDomainTree = "domain.tree"
 
-	// CmdGroupAddAddress is used to add an address to a group
+	// CmdGroupAddAddress is the command to add an address to a group on a domain
 	CmdGroupAddAddress = "group.add"
 
-	// CmdGroupRemoveAddress is used to remove an address from a group
+	// CmdGroupRemoveAddress is the command to remove an address from a group on a domain
 	CmdGroupRemoveAddress = "group.remove"
 
-	// CmdGroupGetAddresses is used to get all the addresseses from a group
+	// CmdGroupGetAddresses is the command to retrieve all addresses from a group on a domain
 	CmdGroupGetAddresses = "group.get"
 
-	// CmdEnvGet is the environment get function
+	// CmdEnvGet is the command to retrieve environment details
 	CmdEnvGet = "env.get"
 
-	// CmdAddressStatesGet is used to get the last state map of an address
+	// CmdAddressStatesGet is the command to get the last known state map of a specific address
 	CmdAddressStatesGet = "address.states.get"
 
-	// CmdAddressGetGroups is used to get the groups of an address
+	// CmdAddressGetGroups is the command to retrieve the groups associated with a specific address
 	CmdAddressGetGroups = "address.groups.get"
 
-	// CmdLocalRulesGet is used to get the local rules for the given client
+	// CmdAddressRulesGet is the command to retrieve the local rules associated with a specific address
 	CmdAddressRulesGet = "address.rules.get"
 
-	// CmdLocalRulesUpdate is used to update the local rules for the given client
+	// CmdAddressRulesUpdate is the command to update the local rules for a specific address
 	CmdAddressRulesUpdate = "address.rules.update"
 
-	// CmdLocalRulesUpdate is used to update the local rules for the given client
+	// CmdAddressTokenReset is the command to reset the token for a specific address
 	CmdAddressTokenReset = "address.token.reset"
 
-	// CmdAddressCustomDataGet is used to get the custom data for the given client
+	// CmdAddressConfigGet is the command to retrieve the custom configuration data for a specific address
 	CmdAddressConfigGet = "address.config.get"
 
-	// CmdAddressCustomDataUpdate is used to update the custom data for the given client
+	// CmdAddressConfigUpdate is the command to update the custom configuration data for a specific address
 	CmdAddressConfigUpdate = "address.config.update"
 
-	// CmdAddressDisable is used to restrict the access to an address
+	// CmdAddressDisable is the command to restrict access to a specific address
 	CmdAddressDisable = "address.disable"
 
-	// CmdAddressDomainGet is used to get the domain assigned to an address
+	// CmdAddressDomainGet is the command to retrieve the domain associated with a specific address
 	CmdAddressDomainGet = "address.domain.get"
 
-	// CmdAddressAliasGet is used to get the aliases assigned to an address
+	// CmdAddressAliasGet is the command to retrieve all aliases associated with a specific address
 	CmdAddressAliasGet = "address.alias.get"
 
-	// CmdAddressAliasAdd is used to add an alias to an address
+	// CmdAddressAliasAdd is the command to add a new alias to a specific address
 	CmdAddressAliasAdd = "address.alias.add"
 
-	// CmdAddressAliasRemove is used to remove an alias from an address
+	// CmdAddressAliasRemove is the command to remove an alias from a specific address
 	CmdAddressAliasRemove = "address.alias.remove"
 
-	// CmdEventsCreate is used to create a new event on the server linked to the source address' domain
+	// CmdEventsCreate is the command to create a new event linked to the source address's domain
 	CmdEventsCreate = "events.create"
 
-	// CmdEventsGet is used to get all events from a domain, since a timestamp
+	// CmdEventsGet is the command to retrieve all events from a domain, since a timestamp
 	CmdEventsGet = "events.get"
 
-	// CmdEventsCreate is used to create a new event on the server linked to the source address' domain
+	// CmdSchemasCreate is the command to create a new schema
 	CmdSchemasCreate = "schemas.create"
 
-	// CmdEventsGet is used to get all events from a domain, since a timestamp
+	// CmdSchemasGet is the command to retrieve schemas from a domain
 	CmdSchemasGet = "schemas.get"
 
+	// CmdSessionDelete is the command to delete a session
 	CmdSessionDelete = "session.delete"
 )
